6b02442a2b55c505320d99d1780965d2782ce949
austin-thwoo <austin-thwoo@naver.com>
2023-03-01 20:43:48 +0900

    📰[feat] : user base create[#1] user domain- base userApi.Class-base userService.Class-base
    author : Noah
    Ref: [#1]
    Resolves:
    see also:

---
+++ src/main/java/com/codingfist/burninghouseuser/user/api/UserApi.java
@@ -0,0 +1,15 @@
+package com.codingfist.burninghouseuser.user.api;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@EnableAutoConfiguration
+@RequiredArgsConstructor
+//@Api(value = "미인증 사용자")
+@RequestMapping("/auth")
+public class UserApi {
+
+}
---
+++ src/main/java/com/codingfist/burninghouseuser/user/domain/User.java
@@ -0,0 +1,25 @@
+package com.codingfist.burninghouseuser.user.domain;
+
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Entity
+@Table(name = "T_USER")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+
+
+
+}
