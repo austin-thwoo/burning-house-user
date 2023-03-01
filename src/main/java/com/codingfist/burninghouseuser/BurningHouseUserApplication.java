2abe49632965e37bace452e33ae324656e2f29d0
austin-thwoo <austin-thwoo@naver.com>
2023-03-01 19:49:51 +0900

    first commit

---
+++ src/main/java/com/codingfist/burninghouseuser/BurningHouseUserApplication.java
@@ -0,0 +1,13 @@
+package com.codingfist.burninghouseuser;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BurningHouseUserApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BurningHouseUserApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/codingfist/burninghouseuser/BurningHouseUserApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codingfist.burninghouseuser;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BurningHouseUserApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
