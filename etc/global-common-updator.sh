e0d8cdeaf1d65fd295b19fe80b54af1050f8bbf4
austin-thwoo <austin-thwoo@naver.com>
2023-03-27 13:32:04 +0900

    🎨feat:shell-script path update

--- etc/global-common-updator.sh
+++ etc/global-common-updator.sh
@@ -2,20 +2,20 @@
 echo "======================================="
 echo "        code-fist helper start..        "
 echo "======================================="
-cd ../src/main/java
-globalcommon=(`ls -a | grep 'burning-house-global-common'`)
+cd ../src/main/java/*/*/*
+globalcommon=(`ls -a | grep 'com.codingfist.burninghouseuser.burning-house-global-common'`)
 if [ -z ${globalcommon} ]; then
   echo "there is no global-common directory.."
   echo "make global-common.."
-  mkdir burning-house-global-common
+  mkdir com.codingfist.burninghouseuser.burning-house-global-common
 fi
-cd burning-house-global-common
+cd com.codingfist.burninghouseuser.burning-house-global-common
 ls=(`ls -a | grep '.git'`)
 if [ -z ${ls} ]; then
   echo "global common directory is not clone directory.."
   echo "remove global common .."
   cd ..
-  rm -rf burning-house-global-common
+  rm -rf com.codingfist.burninghouseuser.burning-house-global-common
   echo "start clone.."
   git clone https://github.com/austin-thwoo/burning-house-global-common.git
 else
